525f11a54a8483c59397cf332e7bc36447aa0557
Kellie Banzon <kelliebanzon@gmail.com>
2018-08-20 14:45:43 -0700

    check indentation - checks for all errors except comments (E114, E115, E116)

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
 from pydriller import GitRepository
 from pycodestyle import *
 
+INDENT_ERRORS = ["E111", "E112", "E113", "E121", "E122", "E123", "E124",
+                 "E125", "E126", "E127", "E128", "E129", "E131", "E133"]
 TABS_SPACES_ERRORS = ["E101", "E223", "E224", "E242", "E273", "E274", "W191"]
 LINE_LENGTH_ERRORS = ["E501"]
 BLANK_LINE_ERRORS = ["E301", "E302", "E303", "E304", "E305", "E306"]
@@ -21,11 +23,13 @@ for f in files:
         pass
         #print(f)"""
 
+
 def error_msg(report, prefix):
     msg = report.get_statistics(prefix)[0]
     index = msg.index(prefix)
     fin = msg[index+len(prefix)+1:]
     return fin
+
 
 def check_errors(counters, report, header, macro, clean):
     print("{} Errors:".format(header))
@@ -33,9 +37,15 @@ def check_errors(counters, report, header, macro, clean):
     for err in macro:
         if err in counters:
             has_errs = True
-            print("    {}, {} occurrence(s): {}".format(err, counters[err], error_msg(report, err)))
+            print("    {}, {} occurrence{} {}".format(err, counters[err],
+                                                       "s:" if counters[err] > 1 else ": ",
+                                                       error_msg(report, err)))
     if not has_errs:
-        print("    None. {} statements conform to PEP8.".format(clean))
+        print("    None. {} statements conform to PEP 8.".format(clean))
+
+#TODO: write wiki for indents
+def check_indents(counters, report):
+    check_errors(counters, report, "Indentation", INDENT_ERRORS, "Indentations of")
 
 def check_tabs_spaces(counters, report):
     check_errors(counters, report, "Tabs vs. Spaces", TABS_SPACES_ERRORS, "Space-indented")
@@ -43,6 +53,7 @@ def check_tabs_spaces(counters, report):
 def check_line_length(counters, report):
     check_errors(counters, report, "Line Length", LINE_LENGTH_ERRORS, "Line length of")
 
+# TODO: write wiki for blank lines
 def check_blank_lines(counters, report):
     check_errors(counters, report, "Blank Line", BLANK_LINE_ERRORS, "Blank line")
 
@@ -54,12 +65,13 @@ def main():
     # Collect the PEP8 reported errors according to pycodestyle.
     sg = StyleGuide()
     breport = BaseReport(options=sg.options)
-    quiet_checker = Checker("messy/blank_lines.py", report=breport)
+    quiet_checker = Checker("messy/clean.py", report=breport)
     quiet_checker.check_all()
     counters = breport.counters
     print(counters)  # TODO: delete this
     print()
     # Divide the errors into categories
+    check_indents(counters, breport)
     check_tabs_spaces(counters, breport)
     check_line_length(counters, breport)
     check_blank_lines(counters, breport)
