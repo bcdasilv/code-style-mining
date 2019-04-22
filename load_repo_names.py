1a66eb60d4652a1d9a755b1b539ff039398a2027
Bruno da Silva <brunocarreiro@gmail.com>
2019-04-22 15:32:22 -0700

    updating python scripts to search for repo names and also updating the list of repos to be analyzed

--- load_repo_names.py
+++ load_repo_names.py
@@ -3,25 +3,28 @@ import errno
 
 path = '/users/brunodasilva/Documents/code-style-mining/repo_names/*.txt'
 
-v02_file = open('v02.txt', 'a')
-v02_file.write('\n')
+repos_file = open('v04.txt', 'w')
 
 files = glob.glob(path)
 
 file_count = 0
 repo_count = 0
 
+unique_repos = set()
 for fname in files:
-    try:
-        with open(fname) as f:
-        	file_count+=1
-        	lines = f.readlines()
-        	for i in range(len(lines)):
-        		repo = lines[i]
-        		v02_file.write(repo)
-        		repo_count+=1
-    except IOError as exc:
-        if exc.errno != errno.EISDIR:
-            raise
+	try:
+		with open(fname) as f:
+			file_count+=1
+			lines = f.readlines()
+			for i in range(len(lines)):
+				repo = lines[i]
+				if repo not in unique_repos:
+					repos_file.write(repo)
+					repo_count+=1
+					unique_repos.add(repo)
+	except IOError as exc:
+		if exc.errno != errno.EISDIR:
+			raise
 print(file_count,' files.\n')
 print(repo_count,' repos added for analysis.')
+repos_file.close()
