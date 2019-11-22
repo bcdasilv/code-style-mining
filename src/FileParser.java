afbd81142e7776f129a3e045fd0d53e5e8a6e98e
Bryce Vonilten <ki6bpd@gmail.com>
2019-11-22 10:44:22 -0800

    Imported Map as list instead of rolling it up with *

--- src/FileParser.java
+++ src/FileParser.java
@@ -1,5 +1,9 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseProblemException;
