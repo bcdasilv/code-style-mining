e22de5ab22ecad28949fc3c3d94a9b0fa650dcf7
Bryce Vonilten <ki6bpd@gmail.com>
2019-12-11 11:26:53 -0800

    Moved file loading into main() and added CL input for missing credentials (part 1)

--- src/MainParser.java
+++ src/MainParser.java
@@ -1,4 +1,15 @@
+import config.Config;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class MainParser {
 //	public static final String gitURL = "https://github.com/janani-sridhar/CaesarCipher";
@@ -7,10 +18,68 @@ public class MainParser {
 //
 //	public static FileParser fp;
 //	public static JSONifySummary summary;
-	
+
+
+	public static void testTermination(String s) {
+		if(s.equals("q")) {
+			System.exit(0);
+		}
+	}
+
 	public static void main(String[] args) throws JSONException {
 		//summary = new JSONifySummary();
 		//fp = new FileParser(summary);
+		Scanner reader = new Scanner(System.in);
+
+		Map<String, String> configMap = new HashMap<>();
+		configMap.put("authToken", null);
+		configMap.put("mongoUrl", null);
+		configMap.put("mongoUsername", null);
+		configMap.put("mongoPassword", null);
+		configMap.put("mongoDatabase", null);
+		configMap.put("mongoCollection", null);
+		configMap.put("tempJavaFilePath", null);
+		configMap.put("tempJSONFilePath", null);
+		configMap.put("repoURLsPath", null);
+
+		Parameters params = new Parameters();
+		FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+				new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+						.configure(params.properties().setFileName("javaAnalysis.properties"));
+		try {
+			Configuration config = builder.getConfiguration();
+
+			configMap.put("tempJavaFilePath", config.getString("tempJavaFilePath").replaceAll("\"", ""));
+			configMap.put("tempJSONFilePath", config.getString("tempJSONFilePath").replaceAll("\"", ""));
+			configMap.put("authToken", config.getString("authToken").replaceAll("\"", ""));
+			configMap.put("repoURLsPath", config.getString("repoURLsPath").replaceAll("\"", ""));
+			configMap.put("mongoUsername", config.getString("mongoUsername"));
+			configMap.put("mongoPassword", config.getString("mongoPassword"));
+			configMap.put("mongoUrl", config.getString("mongoUrl"));
+			configMap.put("mongoDatabase", config.getString("mongoDatabase"));
+			configMap.put("mongoCollection", config.getString("mongoCollection"));
+		}
+		catch(ConfigurationException e) {
+			System.out.println("No javaAnalysis.properties found...\n");
+			//e.printStackTrace();
+		}
+
+		for (Map.Entry<String, String> entry : configMap.entrySet()) {
+			String key = entry.getKey();
+
+			while (configMap.get(key) == null || configMap.get(key).isEmpty()) {
+				System.out.print("Missing " + key + ". Enter one or [q] to quit: ");
+				String s = reader.nextLine().trim();
+				System.out.print("\n");
+				testTermination(s.toLowerCase());
+				configMap.put(key, s);
+			}
+		}
+
+		Config.init(configMap.get("authToken"), configMap.get("mongoUsername"), configMap.get("mongoPassword"),
+				configMap.get("mongoUrl"), configMap.get("mongoDatabase"), configMap.get("mongoCollection"),
+				configMap.get("tempJavaFilePath"), configMap.get("tempJSONFilePath"), configMap.get("repoURLsPath"));
+
 		RepoTraversal traverser = new RepoTraversal();
 
 		traverser.findJavaFilesToParse();
